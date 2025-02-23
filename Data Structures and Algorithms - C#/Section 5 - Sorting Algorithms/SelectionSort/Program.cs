ebe4c1c0c69a4a3498e82f8e1d14b56b246ad027
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-02-23 21:11:42 +0200

    Section - 5 Sorting Algorithms added

---
+++ Data Structures and Algorithms - C#/Section 5 - Sorting Algorithms/SelectionSort/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SelectionSort
+{
+    internal class Program
+    {
+        public static int[] SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int position = i;
+
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[position])
+                    {
+                        position = j;
+                    }
+
+                    int temp = arr[i];
+                    arr[i] = arr[position];
+                    arr[position] = temp;
+                }
+            }
+
+            return arr;
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            Console.Write("Array: ");
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 53, 17, 92, 10 };
+
+            SelectionSort(arr);
+
+            PrintArray(arr);
+        }
+    }
+}
