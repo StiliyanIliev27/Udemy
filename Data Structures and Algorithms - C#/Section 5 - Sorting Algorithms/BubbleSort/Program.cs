b9858e47566c7d7cbf50fef2719b2a1181f819d1
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-02-24 01:13:28 +0200

    Implemented Bubble Sort algorithm

--- Data Structures and Algorithms - C#/Section 5 - Sorting Algorithms/BubbleSort/Program.cs
+++ Data Structures and Algorithms - C#/Section 5 - Sorting Algorithms/BubbleSort/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        public static int[] BubbleSort(int[] arr)
+        {
+            for(int i = arr.Length - 1; i >= 0; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (arr[j + 1] < arr[j])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            Console.Write("Array: ");
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = { 13, 134, 312, 50, 2, 58 };
+
+            BubbleSort(arr);
+
+            PrintArray(arr);
         }
     }
 }
