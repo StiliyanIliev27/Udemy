7c6d71c45e0bf7880dc2fd6017ff3432d809f2d9
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-02-25 18:47:53 +0200

    Added Shell Sort algorithm

---
+++ Data Structures and Algorithms - C#/Section 5 - Sorting Algorithms/ShellSort/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ShellSort
+{
+    internal class Program
+    {
+        public static int[] ShellSort(int[] arr)
+        {
+            int gap = arr.Length / 2;
+
+            while(gap > 0)
+            {
+                for(int i = gap; i< arr.Length; i++) 
+                {
+                    int temp = arr[i];
+                    int j = i - gap;
+
+                    while(j >= 0 && arr[j] > temp)
+                    {
+                        arr[j + gap] = arr[j];
+                        j -= gap;
+                    }
+
+                    arr[j + gap] = temp;
+                }       
+                gap /= 2;
+            }
+
+            return arr;
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            Console.Write("Array: ");
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 13, 124, 50, 34, 2 };
+
+            ShellSort(arr);
+
+            PrintArray(arr);
+        }
+    }
+}
