ec6636aaee58ef2b5fb9c649a65ddfc399659b1f
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-02-28 01:13:40 +0200

    added some new methods to linked list structure

--- Data Structures and Algorithms - C#/Section 6 - Linked List/Linked_List/LinkedList.cs
+++ Data Structures and Algorithms - C#/Section 6 - Linked List/Linked_List/LinkedList.cs
@@ -1,6 +1,6 @@
 ﻿namespace Linked_List
 {
-    public class LinkedList<T>
+    public class LinkedList<T> where T : IComparable<T>
     {
         private Node<T>? head;
         private Node<T>? tail;
@@ -48,7 +48,7 @@
 
         public void AddAny(T element, int position)
         {
-            if(position <= 0 || position >= size)
+            if(position <= 0 || position >= Length)
             {
                 Console.WriteLine("Invalid position!");
                 return;
@@ -56,7 +56,7 @@
 
             Node<T> newestEl = new Node<T>(element);
             Node<T> currentEl = head!;
-            int i = 0;
+            int i = 1;
 
             while(i < position - 1)
             {
@@ -68,6 +68,130 @@
             currentEl.next = newestEl;
             size++;
         }
+
+        public T RemoveFirst()
+        {
+            if(Length == 0)
+            {
+                Console.WriteLine("List is empty!");
+                return default!;
+            }
+
+            T element = head!.element;
+            head = head.next;
+            size--;
+
+            if(Length == 0)
+            {
+                tail = null;
+            }
+
+            return element;
+        }
+
+        public T RemoveLast()
+        {
+            if (Length == 0)
+            {
+                Console.WriteLine("List is empty!");
+                return default!;
+            }
+
+            Node<T> node = head!;
+            int i = 1;
+
+            while(i < Length - 1)
+            {
+                node = node.next!;
+                i++;
+            }
+
+            tail = node;
+            node = node.next!;
+
+            T elementValue = node.element;
+            tail.next = null;
+            size--;
+
+            return elementValue;
+        }
+
+        public T RemoveAny(int position)
+        {
+            if (position <= 0 || position >= Length)
+            {
+                Console.WriteLine("Invalid position!");
+                return default!;
+            }
+
+            Node<T> node = head!;
+            int i = 1;
+
+            while(i < position - 1)
+            {
+                node = node.next!;
+                i++;
+            }
+
+            T element = node.next!.element;
+            node.next = node.next!.next;
+            size--;
+
+            return element;
+        }
+
+        public int Search(T key)
+        {
+            Node<T> node = head!;
+            int index = 0;
+
+            while (node != null)
+            {
+                if(EqualityComparer<T>.Default.Equals(node.element, key))
+                {
+                    return index;
+                }
+
+                node = node.next!;
+                index++;
+            }
+
+            return -1;
+        }
+
+        public void InsertSorted(T element)
+        {
+            Node<T> newest = new Node<T>(element);
+
+            if (Length == 0)
+            {
+                head = newest;
+            }
+            else
+            {
+                Node<T> p = head!;
+                Node<T> q = head!;
+
+                while(p != null && IsGreaterThan(element, p.element))
+                {
+                    q = p;
+                    p = p.next!;
+                }
+
+                if(p == head)
+                {
+                    newest.next = head;
+                    head = newest;
+                }
+                else
+                {
+                    newest.next = q.next;
+                    q.next = newest;
+                }
+            }
+
+            size++;
+        }
         public void Display()
         {
             Node<T>? node = head;
@@ -77,6 +201,11 @@
                 node = node.next;
             }
             Console.WriteLine();
+        }
+
+        public static bool IsGreaterThan(T a, T b)
+        {
+            return a.CompareTo(b) > 0;
         }
     }
 }
--- Data Structures and Algorithms - C#/Section 6 - Linked List/Linked_List/Program.cs
+++ Data Structures and Algorithms - C#/Section 6 - Linked List/Linked_List/Program.cs
@@ -6,12 +6,23 @@
         {
             LinkedList<int> list = new LinkedList<int>();
 
-            list.AddLast(3);
-            list.AddLast(4);
-            list.AddLast(5);
-            list.AddLast(6);
-            list.AddAny(2, 3);
+            //list.AddLast(3);
+            //list.AddLast(4);
+            //list.AddLast(5);
+            //list.AddLast(6);
+
+            list.InsertSorted(5);
+            list.InsertSorted(60);
+            list.InsertSorted(2);
+            list.InsertSorted(1);
+
             list.Display();
+
+            //Console.WriteLine("Removed element: " + list.RemoveLast());
+            //Console.WriteLine("Removed element: " + list.RemoveAny(2));
+            //list.Display();
+
+            Console.WriteLine("Index of searched element: " + list.Search(5));
         }
     }
 }
