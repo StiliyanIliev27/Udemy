9017643aa26fd9b86acaf25cd9355f42aa6c6223
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-03-01 21:49:13 +0200

    Implemented stack with array

---
+++ Data Structures and Algorithms - C#/Section 9 - Stacks/Stack_Using_Array/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Stack_Using_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            StackArray<int> stackArray = new(n);
+
+            stackArray.Push(1);
+            stackArray.Push(2);
+            stackArray.Push(3);
+            stackArray.Push(4);
+
+            Console.Write("Stack elements: ");
+            stackArray.Display();
+
+            int removedEl = stackArray.Pop();
+            Console.WriteLine($"Removed element: {removedEl}");
+
+            Console.WriteLine($"Peek element: {stackArray.Peek()}");
+
+            stackArray.Display();
+
+            Console.WriteLine($"Length of the stack: {stackArray.Length()}");
+        }
+    }
+}
---
+++ Data Structures and Algorithms - C#/Section 9 - Stacks/Stack_Using_Array/StackArray.cs
@@ -0,0 +1,69 @@
+﻿namespace Stack_Using_Array
+{
+    public class StackArray<T>
+    {
+        T[] data;
+        int top;
+
+        public StackArray(int n)
+        {
+            data = new T[n];
+            top = 0;
+        }
+
+        public int Length() => data.Length;
+        private bool IsEmpty()
+        {
+            return top == 0;
+        }
+
+        private bool IsFull()
+        {
+            return top == data.Length;
+        }
+
+        public void Push(T element)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("The stack is full!");
+                return;
+            }
+
+            data[top] = element;
+            top++;
+        }
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The stack is empty!");
+                return default!;
+            }
+
+            T element = data[top -1];
+            top--;
+
+            return element;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The stack is empty!");
+                return default!;
+            }
+
+            return data[top - 1];
+        }
+        public void Display()
+        {
+            for(int i = 0; i < top; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
