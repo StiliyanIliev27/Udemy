4289981ecfdbd9afc97f6340812650afa4660f2f
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-03-02 15:36:36 +0200

    Implemented queue with an array

---
+++ Data Structures and Algorithms - C#/Section 10 - Queues/Queues_Using_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Queues_Using_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            QueueArray<int> queueArray = new(5);
+
+            queueArray.Enqueue(1);
+            queueArray.Enqueue(2);
+            queueArray.Enqueue(3);
+            queueArray.Enqueue(4);
+            queueArray.Enqueue(5);
+
+            queueArray.Dequeue();
+            int peekNumber = queueArray.Peek();
+            Console.WriteLine(peekNumber);
+
+            queueArray.Display();
+        }
+    }
+}
---
+++ Data Structures and Algorithms - C#/Section 10 - Queues/Queues_Using_Array/QueueArray.cs
@@ -0,0 +1,70 @@
+﻿namespace Queues_Using_Array
+{
+    public class QueueArray<T>
+    {
+        private T[] data;
+        private int rear;
+        private int front;
+        private int size;
+
+        public QueueArray(int n)
+        {
+            data = new T[n];
+            rear = 0;
+            front = 0;
+            size = 0;
+        }
+
+        public int Lenght => size;
+
+        private bool IsEmpty() => size == 0;
+        private bool IsFull() => size == data.Length;
+        public void Enqueue(T element)
+        {
+            if (IsFull()) 
+            {
+                Console.WriteLine("The queue is full!");
+                return;
+            }
+
+            data[rear] = element;
+            rear++;
+            size++;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+                return default!;
+            }
+
+            T element = data[front];
+            front++;
+            size--;
+
+            return element;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+                return default!;
+            }
+
+            return data[front];
+        }
+
+        public void Display()
+        {
+            for(int i = front; i < rear; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
